ab056b928d4297c8b0c08fd802662f37a8172b15
brahmi413 <brahmiece413@gmail.com>
2015-07-20 17:38:59 +0530

    sdfljksdflj

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Auto Save Draft | RRPowered</title>
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/rrpowered-autosavedraft.css" />
+    <script rel="text/javascript" src="javascript/jquery.js"></script>
+    <script rel="text/javascript" src="javascript/rrpowered-autosavedraft.js"></script>
+    
+</head>
+<body>
+    <div class="center">
+        <div class="saved"></div>
+        <form>
+            <input type="text" name="title" placeholder="Title" autofocus>
+            <textarea type="text" name="body" placeholder="Body"></textarea>
+            <input type="submit" value="Send">
+        </form>
+    </div>
+</body>
+</html>
